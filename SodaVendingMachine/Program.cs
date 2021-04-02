b765fb7c58f7c802798beb5c1c1e13c2215bafce
The1Moxie <81781818+The1Moxie@users.noreply.github.com>
2021-04-02 21:01:42 +0100

    Add project files.

---
+++ SodaVendingMachine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SodaVendingMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
