dc7aa78b14b9e65ae761c343eaa24fa51b4fb9c8
The1Moxie <81781818+The1Moxie@users.noreply.github.com>
2021-04-04 21:09:28 +0100

    code for the console vending machine

--- SodaVendingMachine/Program.cs
+++ SodaVendingMachine/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using VendingMachine_Library;
 
 namespace SodaVendingMachine
@@ -7,7 +8,28 @@ namespace SodaVendingMachine
     {
         static void Main(string[] args)
         {
+            IMachine vm = new Machine();
 
+            vm.CreateDrink("Coke", 1.99);
+            vm.CreateDrink("Pepsi", 1.19);
+
+            bool exit = false;
+            while (!exit)
+
+            {
+                int i = 1;
+                List<Drink> drinks = vm.GetDrinks();
+                foreach ( Drink drink in drinks)
+                {
+                    Console.WriteLine($"{i}: {drink.Name} - {drink.Price} t-{drink.DrinkID}");
+                    i++;
+                }
+                Console.WriteLine("Select your Drink by Number:");
+                string choice = Console.ReadLine();
+                Drink pickedDrink = drinks.ToArray()[int.Parse(choice)-1];
+                Console.WriteLine($"You bouts a {pickedDrink.Name} for ${pickedDrink.Price} t-{pickedDrink.DrinkID}");
+                vm.DeleteDrink(pickedDrink.DrinkID);
+            }
         }
     }
 }
