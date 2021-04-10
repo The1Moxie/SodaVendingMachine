86ec5694f1e95f04ff3bd45079ce3744551ab523
The1Moxie <81781818+The1Moxie@users.noreply.github.com>
2021-04-10 17:07:50 +0100

    Update Program.cs
    - removed old way of adding drinks as it filled up the db with duplicates.
    - removed test variables that was used to test.
    - added new methods and logic to add drinks and refill when out of stock.

--- SodaVendingMachine/Program.cs
+++ SodaVendingMachine/Program.cs
@@ -8,27 +8,51 @@ namespace SodaVendingMachine
     {
         static void Main(string[] args)
         {
-            IMachine vm = new Machine();
+            
 
-            vm.CreateDrink("Coke", 1.99);
-            vm.CreateDrink("Pepsi", 1.19);
+            
 
             bool exit = false;
             while (!exit)
 
             {
+                IMachine vm = new Machine();
                 int i = 1;
                 List<Drink> drinks = vm.GetDrinks();
                 foreach ( Drink drink in drinks)
                 {
-                    Console.WriteLine($"{i}: {drink.Name} - {drink.Price} t-{drink.DrinkID}");
+                    Console.WriteLine($"{i}: {drink.Name} - ${drink.Price} Quantity: {drink.Quantity}");
                     i++;
                 }
                 Console.WriteLine("Select your Drink by Number:");
                 string choice = Console.ReadLine();
-                Drink pickedDrink = drinks.ToArray()[int.Parse(choice)-1];
-                Console.WriteLine($"You bouts a {pickedDrink.Name} for ${pickedDrink.Price} t-{pickedDrink.DrinkID}");
-                vm.DeleteDrink(pickedDrink.DrinkID);
+                if (choice.StartsWith("a/add"))
+                {
+                    CreateDrinks(vm);
+                }
+                else if (choice.StartsWith("a/refill"))
+                {
+                    Refill(vm);
+                }
+                else
+                {
+                    Drink pickedDrink = drinks.ToArray()[int.Parse(choice) - 1];
+                    Console.WriteLine($"You bought a {pickedDrink.Name} for ${pickedDrink.Price}");
+                    vm.buyDrink(pickedDrink.DrinkID);
+                }
+                
+            }
+        }
+        static void CreateDrinks(IMachine vm) 
+        {
+            vm.CreateDrink("Coke", 1.99, 10);
+            vm.CreateDrink("Pepsi", 1.19, 10);
+        }
+        static void Refill(IMachine vm) 
+        {
+            foreach (Drink drink in vm.GetDrinks())
+            {
+                drink.Quantity = 10;
             }
         }
     }
