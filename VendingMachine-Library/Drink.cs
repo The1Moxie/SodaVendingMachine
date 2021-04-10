6cd3af1ce63bbd29dcd5145cc6a6ecbb070e1144
The1Moxie <81781818+The1Moxie@users.noreply.github.com>
2021-04-10 17:08:05 +0100

    added quantities to drinks

--- VendingMachine-Library/Drink.cs
+++ VendingMachine-Library/Drink.cs
@@ -13,16 +13,18 @@ namespace VendingMachine_Library
         public int DrinkID { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
+        public int Quantity { get; set; }
 
         public Drink()
         { 
 
         }
 
-        public Drink(string Name, double Price)
+        public Drink(string Name, double Price, int Quantity)
         {
             this.Name = Name;
             this.Price = Price;
+            this.Quantity = Quantity;
         }
     }
 }
